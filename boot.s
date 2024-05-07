0c3c573918d8da9c910b016fcdf82e8db2520ce5
cosmic <144744086+ThatGuyAstral@users.noreply.github.com>
2024-05-07 17:07:04 -0400

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,22 +1,33 @@
-.set MAGIC, 0x1BADB002
+.set MAGIC, 0x1badb002
 .set FLAGS, (1<<0 | 1<<1)
 .set CHECKSUM, -(MAGIC + FLAGS)
 
 .section .multiboot
-  .long MAGIC
-  .long FLAGS
-  .long CHECKSUM
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
 
 .section .text
-.extern kmain
+.extern kernelMain
+.extern callConstructors
 .global boot
+
+
 boot:
-  mov $kstack, %esp
-  call kmain
+    mov $kstack, %esp
+    call callConstructors
+    push %eax
+    push %ebx
+    call kernelMain
+
+
 _stop:
-  cli
-  hlt
-  jmp _stop
+    cli
+    hlt
+    jmp _stop
+
+
 .section .bss
-.space 2*1024*1024
+.space 2*1024*1024; # 2 MiB
 kstack:
