5b7064eb4d1cd128b8364c5494f3e37183cfc59f
cosmic <144744086+ThatGuyAstral@users.noreply.github.com>
2024-05-06 21:51:09 -0400

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,1 +1,22 @@
+.set MAGIC, 0x1BADB002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
 
+.section .multiboot
+  .long MAGIC
+  .long FLAGS
+  .long CHECKSUM
+
+.section .text
+.extern kmain
+.global boot
+boot:
+  mov $kstack, %esp
+  call kmain
+_stop:
+  cli
+  hlt
+  jmp _stop
+.section .bss
+.space 2*1024*1024
+kstack:
