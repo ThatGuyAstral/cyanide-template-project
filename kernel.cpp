b2e8d2b2977cfdd04adbb7bd049cd4919500aa2f
cosmic <144744086+ThatGuyAstral@users.noreply.github.com>
2024-05-06 21:54:48 -0400

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -1,1 +1,12 @@
+void kprint(char* str)
+{
+  unsigned short* VidMem = (unsigned short*)0xB8000;
+  for (int i = 0; str[i] != '\0'; ++i)
+    VidMem[i] = str[i]
+}
 
+void kmain(void* multiboot_structure)
+{
+  kprint("Hello from Cyanide!");
+  while(1);
+}
