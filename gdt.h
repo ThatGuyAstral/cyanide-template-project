f4db5f548e0a75dd28be02659096216bde3553ba
cosmic <144744086+ThatGuyAstral@users.noreply.github.com>
2024-05-16 13:46:31 -0700

    implement GDT, IDT and drivers

--- gdt.h
+++ gdt.h
@@ -1,36 +1,46 @@
-#ifndef __GDT_H
-#define __GDT_H
+#ifndef __CYANIDE__GDT_H
+#define __CYANIDE__GDT_H
 
 #include "types.h"
 
-  class GlobalDescriptorTable {
-    public:
-    	class SegmentDescriptor {
-          private:
-          	uint16_t limit_lo;
-          	uint16_t base_lo;
-          	uint8_t base_hi;
-          	uint8_t type;
-          	uint8_t flags_limit_hi;
-          	uint8_t base_vhi;
-          
-          public:
-          	SegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type);
-          	uint32_t Base();
-          	uint32_t Limit();
+namespace cyanide
+{
+    
+    class GlobalDescriptorTable
+    {
+        public:
 
-    	} __attribute__((packed));
+            class SegmentDescriptor
+            {
+                private:
+                    cyanide::common::uint16_t limit_lo;
+                    cyanide::common::uint16_t base_lo;
+                    cyanide::common::uint8_t base_hi;
+                    cyanide::common::uint8_t type;
+                    cyanide::common::uint8_t limit_hi;
+                    cyanide::common::uint8_t base_vhi;
 
-        SegmentDescriptor nullSegmentSelector;
-        SegmentDescriptor unusedSegmentSelector;
-        SegmentDescriptor codeSegmentSelector;
-        SegmentDescriptor dataSegmentSelector;
+                public:
+                    SegmentDescriptor(cyanide::common::uint32_t base, cyanide::common::uint32_t limit, cyanide::common::uint8_t type);
+                    cyanide::common::uint32_t Base();
+                    cyanide::common::uint32_t Limit();
+            } __attribute__((packed));
 
-        GlobalDescriptorTable();
-        ~GlobalDescriptorTable();
+        private:
+            SegmentDescriptor nullSegmentSelector;
+            SegmentDescriptor unusedSegmentSelector;
+            SegmentDescriptor codeSegmentSelector;
+            SegmentDescriptor dataSegmentSelector;
 
-        uint16_t CodeSegmentSelector();
-        uint16_t DataSegmentSelector();
-  };
+        public:
+
+            GlobalDescriptorTable();
+            ~GlobalDescriptorTable();
+
+            cyanide::common::uint16_t CodeSegmentSelector();
+            cyanide::common::uint16_t DataSegmentSelector();
+    };
+
+}
 
 #endif
