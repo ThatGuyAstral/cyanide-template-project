92a5fd8861500ae036786cc1cebfd4330f2c0195
cosmic <144744086+ThatGuyAstral@users.noreply.github.com>
2024-05-16 13:49:30 -0700

    implement GDT, IDT and drivers

---
+++ Hardware/port.h
@@ -0,0 +1,108 @@
+#ifndef __CYANIDE__HARDWARE__PORT_H
+#define __CYANIDE__HARDWARE__PORT_H
+
+#include "../types.h"
+
+namespace cyanide
+{
+    namespace hardware
+    {
+
+        class Port
+        {
+            protected:
+                Port(cyanide::common::uint16_t portnumber);
+                // FIXME: Must be virtual (currently isnt because the kernel has no memory management yet)
+                ~Port();
+                cyanide::common::uint16_t portnumber;
+        };
+
+        
+        class Port8Bit : public Port
+        {
+            public:
+                Port8Bit(cyanide::common::uint16_t portnumber);
+                ~Port8Bit();
+
+                virtual cyanide::common::uint8_t Read();
+                virtual void Write(cyanide::common::uint8_t data);
+
+            protected:
+                static inline cyanide::common::uint8_t Read8(cyanide::common::uint16_t _port)
+                {
+                    cyanide::common::uint8_t result;
+                    __asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (_port));
+                    return result;
+                }
+
+                static inline void Write8(cyanide::common::uint16_t _port, cyanide::common::uint8_t _data)
+                {
+                    __asm__ volatile("outb %0, %1" : : "a" (_data), "Nd" (_port));
+                }
+        };
+
+        class Port8BitSlow : public Port8Bit
+        {
+            public:
+                Port8BitSlow(cyanide::common::uint16_t portnumber);
+                ~Port8BitSlow();
+
+                virtual void Write(cyanide::common::uint8_t data);
+            protected:
+                static inline void Write8Slow(cyanide::common::uint16_t _port, cyanide::common::uint8_t _data)
+                {
+                    __asm__ volatile("outb %0, %1\njmp 1f\n1: jmp 1f\n1:" : : "a" (_data), "Nd" (_port));
+                }
+
+        };
+
+        class Port16Bit : public Port
+        {
+            public:
+                Port16Bit(cyanide::common::uint16_t portnumber);
+                ~Port16Bit();
+
+                virtual cyanide::common::uint16_t Read();
+                virtual void Write(cyanide::common::uint16_t data);
+
+            protected:
+                static inline cyanide::common::uint16_t Read16(cyanide::common::uint16_t _port)
+                {
+                    cyanide::common::uint16_t result;
+                    __asm__ volatile("inw %1, %0" : "=a" (result) : "Nd" (_port));
+                    return result;
+                }
+
+                static inline void Write16(cyanide::common::uint16_t _port, cyanide::common::uint16_t _data)
+                {
+                    __asm__ volatile("outw %0, %1" : : "a" (_data), "Nd" (_port));
+                }
+        };
+
+        class Port32Bit : public Port
+        {
+            public:
+                Port32Bit(cyanide::common::uint16_t portnumber);
+                ~Port32Bit();
+
+                virtual cyanide::common::uint32_t Read();
+                virtual void Write(cyanide::common::uint32_t data);
+
+            protected:
+                static inline cyanide::common::uint32_t Read32(cyanide::common::uint16_t _port)
+                {
+                    cyanide::common::uint32_t result;
+                    __asm__ volatile("inl %1, %0" : "=a" (result) : "Nd" (_port));
+                    return result;
+                }
+
+                static inline void Write32(cyanide::common::uint16_t _port, cyanide::common::uint32_t _data)
+                {
+                    __asm__ volatile("outl %0, %1" : : "a"(_data), "Nd" (_port));
+                }
+        };
+
+    }
+}
+
+#endif
