feb314d4a6bcc04641916ced8ad607bafc169eb0
cosmic <144744086+ThatGuyAstral@users.noreply.github.com>
2024-05-16 13:53:42 -0700

    implement GDT, IDT and drivers

---
+++ Drivers/driver.h
@@ -0,0 +1,38 @@
+#ifndef __CYANIDE__DRIVERS__DRIVER_H
+#define __CYANIDE__DRIVERS__DRIVER_H
+
+namespace cyanide
+{
+    namespace drivers
+    {
+
+        class Driver
+        {
+        public:
+            Driver();
+            ~Driver();
+            
+            virtual void Activate();
+            virtual int Reset();
+            virtual void Deactivate();
+        };
+
+        class DriverManager
+        {
+        private:
+            Driver* drivers[265];
+            int numDrivers;
+            
+        public:
+            DriverManager();
+            void AddDriver(Driver*);
+            
+            void ActivateAll();
+            
+        };
+        
+    }
+}
+    
+    
+#endif
